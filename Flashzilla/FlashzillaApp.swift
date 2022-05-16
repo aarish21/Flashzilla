a40ff7853d3653cc3ce6c0d528fcbe8e7a3ad2ac
aarish <aarish.jerry@gmail.com>
2022-05-16 13:52:07 +0530

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Aarish on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
