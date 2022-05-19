a7cb5cfcb6c3f12750154fa96241995c11a8a1e3
aarish <aarish.jerry@gmail.com>
2022-05-19 23:43:10 +0530

    initial changes

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
-
+import CoreHaptics
 struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
     }
 }
